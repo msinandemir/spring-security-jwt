9373606a07f52be088cddc5d111c7f6857f7deea
Sinan <sinandeemir@gmail.com>
2024-05-16 21:26:21 +0300

    refactor: added builder annatotion

--- src/main/java/com/msdemir/springsecurityjwt/dtos/responses/AddUserResponse.java
+++ src/main/java/com/msdemir/springsecurityjwt/dtos/responses/AddUserResponse.java
@@ -1,7 +1,10 @@
 package com.msdemir.springsecurityjwt.dtos.responses;
 
+import lombok.Builder;
+
 import java.time.Instant;
 
+@Builder
 public record AddUserResponse(Long id,
                               Instant createdAt,
                               Instant updatedAt,
