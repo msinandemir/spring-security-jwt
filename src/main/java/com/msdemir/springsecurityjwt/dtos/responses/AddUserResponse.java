03a7622d2eef58fe2fe6dc3651bfd90ec2b2332a
Sinan <sinandeemir@gmail.com>
2024-05-16 21:28:37 +0300

    refactor: added authorities field

--- src/main/java/com/msdemir/springsecurityjwt/dtos/responses/AddUserResponse.java
+++ src/main/java/com/msdemir/springsecurityjwt/dtos/responses/AddUserResponse.java
@@ -1,13 +1,17 @@
 package com.msdemir.springsecurityjwt.dtos.responses;
 
+import com.msdemir.springsecurityjwt.models.Role;
 import lombok.Builder;
 
 import java.time.Instant;
+import java.util.List;
 
 @Builder
 public record AddUserResponse(Long id,
                               Instant createdAt,
                               Instant updatedAt,
                               String name,
-                              String username) {
+                              String username,
+                              List<Role> authorities
+) {
 }
