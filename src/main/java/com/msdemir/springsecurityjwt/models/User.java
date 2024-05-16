6d85930ea3cf27157ea10efeabccdf0074c04a46
Sinan <sinandeemir@gmail.com>
2024-05-16 20:51:41 +0300

    added: User entity class with implements UserDetails interface

---
+++ src/main/java/com/msdemir/springsecurityjwt/models/User.java
@@ -0,0 +1,56 @@
+package com.msdemir.springsecurityjwt.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class User extends BaseEntity implements UserDetails {
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false,unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role",nullable = false)
+    @ElementCollection(targetClass = Role.class,fetch = FetchType.EAGER)
+    @JoinTable(name = "roles",joinColumns = @JoinColumn(name = "user_id"))
+    private List<Role> authorities;
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
