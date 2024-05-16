f861079a3e2d3d080c1e68425371c8c2c587150c
Sinan <sinandeemir@gmail.com>
2024-05-16 21:08:42 +0300

    updated: added builder annotation at User class

--- src/main/java/com/msdemir/springsecurityjwt/models/User.java
+++ src/main/java/com/msdemir/springsecurityjwt/models/User.java
@@ -1,10 +1,7 @@
 package com.msdemir.springsecurityjwt.models;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
@@ -17,6 +14,7 @@ import java.util.List;
 @NoArgsConstructor
 @Getter
 @Setter
+@Builder
 public class User extends BaseEntity implements UserDetails {
 
     @Column(nullable = false)
