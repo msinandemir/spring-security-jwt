37773b99a82cfa1aa4b18ee8ca5ec2cc15c09320
Sinan <sinandeemir@gmail.com>
2024-05-16 22:16:42 +0300

    added: jwt util class

---
+++ src/main/java/com/msdemir/springsecurityjwt/utils/JwtUtil.java
@@ -0,0 +1,54 @@
+package com.msdemir.springsecurityjwt.utils;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.Map;
+
+@Service
+public class JwtUtil {
+
+    @Value("${app.jwt-secret}")
+    String JWT_SECRET;
+
+    @Value("${app.jwt-expiration}")
+    Long JWT_EXPIRATION;
+
+    public String generateToken(String username, Map<String, Object> extraClaims) {
+        return Jwts.builder()
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + JWT_EXPIRATION))
+                .setSubject(username)
+                .setClaims(extraClaims)
+                .signWith(getSignKey())
+                .compact();
+    }
+
+    public boolean validateToken(String token) {
+        return getClaimsFromToken(token).getExpiration().after(new Date(System.currentTimeMillis()));
+    }
+
+    public String extractUsernameFromToken(String token) {
+        return getClaimsFromToken(token).getSubject();
+    }
+
+    private Claims getClaimsFromToken(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSignKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignKey() {
+        byte[] keyBytes = Decoders.BASE64URL.decode(JWT_SECRET);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
