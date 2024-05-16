b5388d222bc367446f5b6dcba0adb3037bfc8ac4
Sinan <sinandeemir@gmail.com>
2024-05-16 20:45:29 +0300

    added: Role enum

---
+++ src/main/java/com/msdemir/springsecurityjwt/models/Role.java
@@ -0,0 +1,14 @@
+package com.msdemir.springsecurityjwt.models;
+
+import org.springframework.security.core.GrantedAuthority;
+
+public enum Role implements GrantedAuthority {
+    ROLE_ADMIN,
+    ROLE_USER
+    ;
+
+    @Override
+    public String getAuthority() {
+        return name();
+    }
+}
